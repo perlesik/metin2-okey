d7e0621fd88c2174076bd688fdce8a27b622c5b3
creencepl <65626423+creencepl@users.noreply.github.com>
2020-05-20 02:36:52 +0200

    Update js.js

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ cards.addEventListener('click', reverseCard);
 btn.addEventListener('click', resetCards);
 
 function reverseCard(){
-    let name = event.target.getAttribute('value')
+    let name = event.target.getAttribute('value');
     let txt = event.target.src;
     let tmp = txt.substr(txt.length-10);
     event.target.src = tmp=='img/'+name+'.png' ? 'img/blank.png' : 'img/'+name+'.png';
